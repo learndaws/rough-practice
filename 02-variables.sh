a4282ad05a98a849f83111604204e28dc66955c5
learndaws <learndaws@gmail.com>
2023-12-29 20:17:07 +0530

    first variables push

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+HE1=$1
+SHE2=$2
+
+echo "$HE: Hello $SHE, How are you doing?"
+echo "$SHE: Hi $HE, I'm doing great and how about you?"
+echo "$HE: I'm also good and nice to meet you"
+echo "$SHE: I'm also"
