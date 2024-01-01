75dbad4727eec7b9cc8ac5a015b3e90f2d1e0c7b
learndaws <learndaws@gmail.com>
2024-01-01 13:31:27 +0530

    07

--- 07-read-experiment.sh
+++ 07-read-experiment.sh
@@ -8,5 +8,20 @@ echo "Enter PASSWORD:"
 
 read -s HELLO
 
-echo "Entered username is "${ADMIN}""
-echo "Entered password is "${HELLO}""
+if [ $ADMIN=testadmin ];
+    then 
+        echo "username is correct"
+    else
+        echo "username is incorrect"
+fi 
+
+if [ $HELLO=testpassword ];
+    then 
+        echo "password is correct"
+    else
+        echo "password is incorrect"
+fi 
+
+
+echo "Entered username is ${ADMIN}"
+echo "Entered password is ${HELLO}"
