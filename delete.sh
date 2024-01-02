e4cfb216b694fec36ac3c3ad438fe4e0f1ba0327
learndaws <learndaws@gmail.com>
2024-01-02 20:08:45 +0530

    exp push

--- delete.sh
+++ delete.sh
@@ -3,9 +3,10 @@
 FUNCTION()
 {
     for ((NUMBER=1; NUMBER<=$1; NUMBER++))
+    for ((NUMBER=1; NUMBER<=$2; NUMBER++))
     do
         echo "Number: ${NUMBER}"
     done
 }
 
-FUNCTION $1
+FUNCTION $1 $2
