d88b6a4e6f705086abea673518029f07483a031a
learndaws <learndaws@gmail.com>
2024-01-07 21:23:56 +0530

    13

--- 13-dns-lookup.sh
+++ 13-dns-lookup.sh
@@ -4,6 +4,6 @@ READ_FILE=$(cat 14-IP-Addresses)
 
 while IFS= read -r line
 do 
-    nslookup $line | grep name &>> /home/centos/rough-practice/new.log 
+    nslookup $line | grep name
 
 done <<< $READ_FILE
