979feb24012f82bb3565c86524a695264118dbe7
learndaws <learndaws@gmail.com>
2024-01-07 20:29:42 +0530

    13-dns-lookup

--- 13-dns-lookup.sh
+++ 13-dns-lookup.sh
@@ -2,6 +2,6 @@
 
 while IFS= read -r line
 do 
-    nslookup $line | grep name &> /home/centos/rough-practice/new.log 
+    nslookup $line | grep name &>> /home/centos/rough-practice/new.log 
 
 done < 14-IP-Addresses
