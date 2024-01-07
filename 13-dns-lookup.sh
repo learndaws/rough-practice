e36eea7cd8d6964de784c337f15aac2fcb3d9a80
learndaws <learndaws@gmail.com>
2024-01-07 23:57:03 +0530

    13

--- 13-dns-lookup.sh
+++ 13-dns-lookup.sh
@@ -2,7 +2,16 @@
 
 READ_FILE=$(cat 14-IP-Addresses)
 
+# Clear the existing content of the log file
+> /home/centos/rough-practice/first.log
+
 while IFS= read -r line
 do 
-    nslookup $line | grep name &>> /home/centos/rough-practice/first.log 
-done <<< $READ_FILE
+    nslookup "$line" | awk '/^Name:/ {name=$2} /^Address:/ {ip=$2} END {print ip, name}' >> /home/centos/rough-practice/first.log 
+done <<< "$READ_FILE"
+
+# Print IP addresses and FQDNs
+while read -r IP_Address FQDN
+do 
+    echo "$IP_Address $FQDN"
+done < /home/centos/rough-practice/first.log
