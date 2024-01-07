d795abf879e4272e0dde0332da396449ed3d85b3
learndaws <learndaws@gmail.com>
2024-01-07 23:43:11 +0530

    13

--- 13-dns-lookup.sh
+++ 13-dns-lookup.sh
@@ -5,10 +5,4 @@ READ_FILE=$(cat 14-IP-Addresses)
 while IFS= read -r line
 do 
     nslookup $line | grep name &>> /home/centos/rough-practice/first.log 
-
-done <<< $READ_FILE
-
-while IFS=" " read -r IP_Address name equals FQDN
-do 
-    echo  "$IP_Address $FQDN"
-done < /home/centos/rough-practice/first.log
+done
